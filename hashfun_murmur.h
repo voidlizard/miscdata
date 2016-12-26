e50bad67ae67d6ac2ec25d34a3da6fb005e9c6ef
Dmitry Zuikov <dzuikov@gmail.com>
2016-12-26 15:51:16 +0300

    hashfun murmur3

---
+++ hashfun_murmur.h
@@ -0,0 +1,8 @@
+#ifndef __hashfun_murmur_h
+#define __hashfun_murmur_h
+
+uint32_t hash_murmur3_32( const uint8_t *key
+                        , uint32_t len
+                        , uint32_t seed);
+
+#endif
