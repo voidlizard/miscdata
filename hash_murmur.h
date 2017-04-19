c696cff68e140757fc25e3deab3871bb34d31a10
Dmitry Zuikov <dzuikov@gmail.com>
2017-04-19 13:40:30 +0300

    added hash_murmur

---
+++ hash_murmur.h
@@ -0,0 +1,10 @@
+#ifndef __hash_hash_murmur_h
+#define __hash_hash_murmur_h
+
+#include <stdint.h>
+
+uint32_t hash_murmur3_32( const uint8_t *key
+                        , uint32_t len
+                        , uint32_t seed);
+
+#endif
